9b53d79d995d25cfc5229a7bab34e32057dad7f9
SlashD9 <colin.p.s.daly@gmail.com>
2017-11-12 20:03:22 +0000

    Added bootstrap NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+
 
 
 @NgModule({
